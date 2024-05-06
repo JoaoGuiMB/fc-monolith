ddbcd13ce113a6433fe906cf37f7b2d0fa7a9345
JoaoGuiMB <juaogui159@gmail.com>
2024-05-06 14:17:22 -0300

    feat: implementing create client repo

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "./client.model";
 import ClientRepository from "./client.repository";
+import Client from "../domain/client-entity";
+import Id from "../../@shared/domain/value-object/id.value-object.interface";
 
 describe("ProductRepository test", () => {
   let sequelize: Sequelize;
@@ -21,7 +23,7 @@ describe("ProductRepository test", () => {
   });
 
   it("should find a client", async () => {
-    const client = await ClientModel.create({
+    await ClientModel.create({
       id: "1",
       name: "John Doe",
       email: "aasd@email.com",
@@ -34,5 +36,24 @@ describe("ProductRepository test", () => {
     const result = await clientRepository.find("1");
     expect(result.id.id).toBe("1");
     expect(result.name).toBe("John Doe");
+  });
+
+  it("should create client", async () => {
+    const clientRepository = new ClientRepository();
+    const client = new Client({
+      id: new Id("1"),
+      name: "John Doe",
+      email: "asd@email.com",
+      address: "1234 Main St",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await clientRepository.add(client);
+
+    const result = await ClientModel.findByPk("1");
+    expect(result.id).toBe("1");
+    expect(result.name).toBe("John Doe");
+    expect(result.createdAt).toStrictEqual(client.createdAt);
   });
 });
