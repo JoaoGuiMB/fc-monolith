6171d80f828966b4db013b55761b522fccad98c1
JoaoGuiMB <juaogui159@gmail.com>
2024-05-07 18:40:44 -0300

    feat: add address

---
+++ src/modules/@shared/domain/value-object/address.value-object.ts
@@ -0,0 +1,58 @@
+export default class Address {
+  _street: string = "";
+  _complement: string = "";
+  _number: number = 0;
+  _zipCode: string = "";
+  _city: string = "";
+
+  constructor(
+    street: string,
+    number: number,
+    zipCode: string,
+    city: string,
+    complement: string
+  ) {
+    this._street = street;
+    this._number = number;
+    this._zipCode = zipCode;
+    this._city = city;
+    this._complement = complement;
+
+    this.validate();
+  }
+
+  get street(): string {
+    return this._street;
+  }
+
+  get number(): number {
+    return this._number;
+  }
+
+  get zip(): string {
+    return this._zipCode;
+  }
+
+  get city(): string {
+    return this._city;
+  }
+
+  validate() {
+    if (this._street.length === 0) {
+      throw new Error("Street is required");
+    }
+    if (this._number === 0) {
+      throw new Error("Number is required");
+    }
+    if (this._zipCode.length === 0) {
+      throw new Error("Zip is required");
+    }
+    if (this._city.length === 0) {
+      throw new Error("City is required");
+    }
+  }
+
+  toString() {
+    return `${this._street}, ${this._number}, ${this._zipCode} ${this._city}`;
+  }
+}
